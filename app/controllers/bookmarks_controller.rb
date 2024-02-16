34ecebd7d96ca2fd19426bb68707fda585fa768b
hiroshi nagao <hi_1205@hotmail.com>
2024-02-16 15:22:56 +0900

    add delete

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,13 +1,16 @@
 class BookmarksController < ApplicationController
   def new
     @bookmark = Bookmark.new
+    # @list = list(params[:id])
+    @list = List.find(params[:list_id])
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.save
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
     if @bookmark.save
-      redirect_to list_path(@bookmark)
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
@@ -22,6 +25,6 @@ class BookmarksController < ApplicationController
   private
 
   def bookmark_params
-    params.require(:bookmark).permit(:comment)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
